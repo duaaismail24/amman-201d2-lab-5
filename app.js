0e6130270af83d4c37252e8e456548a47f76eb22
duaaismail24 <doaaee7@gmail.com>
2019-11-16 23:55:20 +0200

    test

--- app.js
+++ app.js
@@ -16,32 +16,24 @@ return [c, 'The sum of ' + a + ' and ' + b + ' is ' + c + '.'];
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
 
+
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 2
 Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
-"The product of 5 and 9 is 45."
+// "The product of 5 and 9 is 45."
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
- //Write your code here
-//  var a = 5 
-//  var b = 9
-// function multiply(a, b) { //eslint-disable-line
-//     var a = 5;
-//     var b = 9;
-//     var product = (a*b);
-
-
-
-
-
-// }
-
+//  Write your code here
+function multiply(a, b) { //eslint-disable-line
+    var x = a * b;
+    return [x, 'The product of ' + a + ' and ' + b + ' is ' + x + '.']
+    }
 // Here is the test for multiply(); uncomment it to run it
-//  testMultiply(5,9);
+ testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -58,11 +50,20 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+    var sum1= sum(a,b);
+    var sum2= sum(sum1[0],c);
+    var multiply1 =multiply(a,b);
+    var multiply2=multiply(multiply1[0],c);
+    var result = [];
+    result.push (sum2[0]);
+    result.push (multiply2[0]);
+    result.push (a + " and " + b + " and " + c + " sum to " + sum2[0] + ".");
+    result.push ("The product of " + a + " and " + b + " and " + c + " is " + multiply2[0] + ".");
+    return result;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
